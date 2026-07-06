82e5b81bcd28b3c68a7a92ac359f6bc3e7378f1b
younkyung-wq <kyung@lazyz.kr>
2026-07-06 16:14:23 +0900

    Favicon star

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageOps
 
 st.set_page_config(
     page_title="LAZYZ Dashboard",
-    page_icon="💤",
+    page_icon="⭐️",
     layout="wide",
     initial_sidebar_state="expanded"
 )
